4945c56d5672b34bcef661a00e53d93c23a80865
Himanshu Sharma <143724283+himanshu7437@users.noreply.github.com>
2025-11-05 17:19:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const colorSelect = document.querySelector("#colorSelect");
+const removeBtn = document.querySelector('input[type="button"]');
+
+removeBtn.addEventListener("click", () => {
+	const selectedIndex = colorSelect.selectedIndex;
+
+	if(selectedIndex !== -1) {
+		colorSelect.remove(selectedIndex);
+	}
+})
